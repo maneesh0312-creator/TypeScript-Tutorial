fe4d1769c5492cff5dea0e89858000c1e3180136
maneesh0312-creator <maneeshkumar031295@gmail.com>
2026-08-17 02:34:05 +0530

    Learning BigInt Data type

---
+++ dist/bigIntType.d.ts
@@ -0,0 +1,4 @@
+declare let bigNumber: bigint;
+declare let bigNum: bigint;
+declare let a: bigint;
+declare let b: bigint;
---
+++ dist/bigIntType.js
@@ -0,0 +1,23 @@
+"use strict";
+// Learning BigInt Type
+// BigInt used to when failed number type
+// BigInt used to banking system
+// BigInt used to scientific calculator
+// BigInt datatype is a special datatype which store the large integer value.
+// BigInt is a primitive datatype.
+// How to verify the BigInt Number
+// If 2 ki power 53-1 is above, so that it is BigInt if less it is number
+// How to check BigInt
+console.log(Number.MAX_SAFE_INTEGER);
+console.log(Number.MAX_SAFE_INTEGER + 1);
+console.log(Number.MAX_SAFE_INTEGER + 2);
+// How to make bigInt Number
+let bigNumber = 1233483654749763n;
+console.log(bigNumber);
+let bigNum = BigInt(23654356373);
+console.log(bigNum);
+// BigInt Operation
+let a = 100n;
+let b = 50n;
+console.log((a + b));
+console.log((a * b));
